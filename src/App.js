ef9ccaf6221d9bad0b0b1089570db09d0f8cecb1
shravan <shravpar98@gmail.com>
2022-02-25 15:18:08 -0500

    aws

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> learning github part 2.
+          Edit <code>src/App.js</code> learning AWS.
         </p>
         <a
           className="App-link"
