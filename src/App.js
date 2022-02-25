b2ec3d82632f8cd1a73d566717b330a73a77025a
shravan <shravpar98@gmail.com>
2022-02-25 11:46:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload testing testing testing 56789101112.
+          Edit <code>src/App.js</code> learning github.
         </p>
         <a
           className="App-link"
