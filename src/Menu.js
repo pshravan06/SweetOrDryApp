96909f5b67b935e732ad7c1d5e75f8e5966b5a06
shravan <shravpar98@gmail.com>
2022-02-27 11:26:38 -0500

    init

---
+++ src/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+
+const Menu = () => {
+  return (
+   <>
+  <Button>Click Here</Button>
+   </>
+  )
+}
+
+export default Menu
